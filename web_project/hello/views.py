1806618ecffe050f17453674942c0da75d3230b9
TheGodfather3 <schoenephilip@gmail.com>
2021-05-17 17:45:18 +0200

    Teil mit Datenbank

--- web_project/hello/views.py
+++ web_project/hello/views.py
@@ -2,6 +2,29 @@ from django.shortcuts import render
 import re
 from django.http import HttpResponse
 from django.utils.timezone import datetime
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+from django.views.generic import ListView
+
+class HomeListView(ListView):
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
 
 def hello_there(request, name):
     return render(
